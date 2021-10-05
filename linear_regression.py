5fdeadb031131fe5b3d660eec050ef31dab18214
Neeshi-M <200050087@iitb.ac.in>
2021-10-05 17:54:06 +0530

    first commit

---
+++ linear_regression.py
@@ -0,0 +1,63 @@
+import csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+df =pd.read_csv('/Users/jaineel/Downloads/1.01. Simple linear regression.csv')
+
+x=[]
+y=[]
+with open('/Users/jaineel/Downloads/1.01. Simple linear regression.csv') as csvdata:
+    items=csv.reader(csvdata)
+    for row in items:
+        x.append(row[0])
+        y.append(row[1])
+#print(x)
+#print(y)
+final_y=[]
+final_x=[]
+
+for i in range(1,len(x)):
+    final_y.append(x[i])
+
+
+
+for j in range(1,len(x)):
+    final_x.append(y[j])
+
+
+
+
+
+
+print(final_y)
+
+print(final_x)
+x=np.array(final_x)
+y=np.array(final_y)
+y=y.astype('float')/1000
+y=y.reshape(y.size,1)
+
+#plt.scatter(x,y )
+#plt.show()
+
+
+x=np.vstack((np.ones((x.size,)),x)).T
+print(x.shape)
+print(y.shape)
+def model(x,y,learningrate,iteration):
+   m=y.size
+   theta=np.zeros((2,1))
+   for i in range(iteration):
+       y_pred=np.dot(x,theta)
+       cost=(1/(2*m))*np.sum(np.square(y_pred-y))
+       d_theta=(1/m)*np.dot(x.T,y_pred-y)
+       theta=theta-learningrate*d_theta
+
+   return theta
+iterations=10000
+learning_rate=0.08
+final_theta=model(x.astype('float'),y.astype('float'),learning_rate,iterations)
+final_theta=final_theta.astype('float')*1000
+
+
+print(np.dot([1,3.17],final_theta))
---
+++ logistic_regression.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import math
+
+
+purchase_data =pd.read_csv("/Users/jaineel/Downloads/User_Data - User_Data (1).csv")
+#print(purchase_data.head(10))
+#sns.count_
+gender=pd.get_dummies(purchase_data["Gender"],drop_first=True)
+#purchase=pd.get_dummies(purchase_data["Purchased"],drop_first=True)
+purchase_data=pd.concat([purchase_data,gender],axis=1)
+#print(purchase_data.head(10))
+purchase_data.drop(['Gender'],axis=1,inplace=True)
+#print(purchase_data.head(10))
+y=purchase_data['Purchased']
+x=purchase_data.drop('Purchased',axis=1)
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.33,random_state=42)
+#print(x_train)
+from sklearn.linear_model import LogisticRegression
+
+logmodel=LogisticRegression()
+logmodel.fit(x_train,y_train)
+predictions=logmodel.predict(x_test)
+from sklearn.metrics import classification_report
+classification_report(y_test,predictions)
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_test,predictions)
+from  sklearn.metrics import accuracy_score
+print(accuracy_score(y_test,predictions))
